07100ce906e423a405572157d845e3115a70743c
abrusu-collins <snillochemingson@gmail.com>
2022-10-27 17:31:10 +0000

    nice one

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -1,10 +1,23 @@
+import _1 from "../img/1.svg";
+import _2 from "../img/2.svg";
+
 function Testimonies() {
-  return <div className="testimonies">
-    <div className="testimonies-head">What our Users say about us</div>
-    <div className="top-quote">
-        <img src="" alt="" />
+  return (
+    <div className="testimonies">
+      <div className="testimonies-inner">
+        <div className="testimonies-head">What our Users say about us</div>
+        <div className="top-quote">
+          <img src={_1} alt="" />
+          <img src={_1} alt="" />
+        </div>
+
+        <div className="bottom-quote">
+          <img src={_2} alt="" />
+          <img src={_2} alt="" />
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Testimonies;
