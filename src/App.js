6111f8d0b850f353d36a011568ebdb520d9453ba
abrusu-collins <snillochemingson@gmail.com>
2022-10-27 17:25:03 +0000

    nice one

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/index.css";
 import Hero from "./components/Hero";
 import Offer from "./components/Offer";
 import Everybody from "./components/Everybody";
+import Testimonies from "./components/Testimonies";
 function App() {
   return (
     <>
       <Hero />
       <Offer/>
       <Everybody/>
+      <Testimonies/>
     </>
   );
 }
---
+++ src/components/Testimonies.jsx
@@ -0,0 +1,10 @@
+function Testimonies() {
+  return <div className="testimonies">
+    <div className="testimonies-head">What our Users say about us</div>
+    <div className="top-quote">
+        <img src="" alt="" />
+    </div>
+  </div>;
+}
+
+export default Testimonies;
