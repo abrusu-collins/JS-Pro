0f4c169dde369ed8f8eb5a1c569b652e53f92fae
abrusu-collins <snillochemingson@gmail.com>
2022-10-27 21:14:41 +0000

    Let's do this

--- src/components/Wait.jsx
+++ src/components/Wait.jsx
@@ -1,7 +1,17 @@
+import rocket from "../img/rocket.png";
 function Wait() {
-    return ( 
-        <div className="wait"></div>
-     );
+  return (
+    <div className="wait">
+      <div className="wait-inner">
+        <div className="wait-about">
+          <p>What are you waiting for?</p>
+          <p>Skyrocket your Javascript Skills now</p>
+          <a href="">Start Learning</a>
+        </div>
+        <img src={rocket} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Wait;
