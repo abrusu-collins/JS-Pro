d65f9a0f446b31998cd5509d54b373461eac8faa
abrusu-collins <snillochemingson@gmail.com>
2022-10-27 15:32:51 +0000

    nice one

--- src/components/Everybody.jsx
+++ src/components/Everybody.jsx
@@ -6,6 +6,12 @@ function Everybody() {
       <di className="everybody-description">
         <p className="head-small">Everyone in mind</p>
         <p className="head-big">No matter where or how you like to learn.</p>
+        <p className="about">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+          sed mollitia labore fuga molestias nihil, placeat distinctio impedit,
+          tempore dignissimos quis nam ab aut praesentium? Laborum commodi
+          architecto consectetur porro!
+        </p>
       </di>
     </div>
   );
