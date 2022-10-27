e8512f5ecc0b1028f0a160b8387cae0bcb7f95c0
abrusu-collins <snillochemingson@gmail.com>
2022-10-27 18:59:37 +0000

    nice one

--- src/components/Free.jsx
+++ src/components/Free.jsx
@@ -1,9 +1,14 @@
 function Free() {
-    return ( 
-        <div className="free">
-            
-        </div>
-     );
+  return (
+    <div className="free">
+      <div className="up">
+        <p>Start leveling up your JavaScript skills. It's free</p>
+      </div>
+      <div className="it-is-free">
+        <p>JS-Pro was created just to help junior developers and newbies </p>
+      </div>
+    </div>
+  );
 }
 
 export default Free;
