a73fa4ec25c757cf2310c4d6430687612533df19
abrusu-collins <snillochemingson@gmail.com>
2022-10-28 12:41:13 +0000

    nice one

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import footergirl from "../img/footer-g.png";
 function Footer() {
     return ( 
         <div className="footer">
-            
+            <div className="footer-inner">
+                <img src={footergirl} alt="" />
+                <div>
+                    <p className="oops">Oops!</p>
+                    <p className="end">You've reached the end.</p>
+                    <p>Till we meet again.</p>
+                    <p>Bye!</p>
+                </div>
+            </div>
+            <div className="abrusu"></div>
         </div>
      );
 }
