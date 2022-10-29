cd78af548ec5fb3d5a4bc6a992d216d23aa5eade
abrusu-collins <snillochemingson@gmail.com>
2022-10-29 13:16:47 +0000

    nice one

--- src/components/Soon.jsx
+++ src/components/Soon.jsx
@@ -1,10 +1,22 @@
-import sadgirl from "../img/sad-girl.png"
+import sadgirl from "../img/sad-girl.png";
 function Soon() {
-    return ( 
-        <div className="soon">
-            <img src={sadgirl} alt="" />
-        </div>
-     );
+  return (
+    <div className="soon">
+      <img src={sadgirl} alt="" />
+      <div className="the-form">
+        <p className="soon-title">Leaving so soon?</p>
+        <form id="form">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your Email"
+          />
+          <button>Subscribe</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Soon;
