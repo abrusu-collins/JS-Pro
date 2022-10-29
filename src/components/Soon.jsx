569049cf9f5c5410d855b3b88da399ceff491958
abrusu-collins <snillochemingson@gmail.com>
2022-10-29 13:19:11 +0000

    nice

--- src/components/Soon.jsx
+++ src/components/Soon.jsx
@@ -5,6 +5,7 @@ function Soon() {
       <img src={sadgirl} alt="" />
       <div className="the-form">
         <p className="soon-title">Leaving so soon?</p>
+        <p className="cta">Subscribe to our News Letter</p>
         <form id="form">
           <input
             type="email"
