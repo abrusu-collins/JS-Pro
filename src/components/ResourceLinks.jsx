24319d634d43f2707a6e7c445473e1091978dc9a
abrusu-collins <snillochemingson@gmail.com>
2022-11-06 16:24:04 +0000

    nice

--- src/components/ResourceLinks.jsx
+++ src/components/ResourceLinks.jsx
@@ -1,10 +1,21 @@
+import frontend from "../img/frontend";
+import backend from "../img/backend";
+import projects from "../img/projects";
+import resume from "../img/resume";
+import linkedin from "../img/linkedin";
+import jobs from "../img/jobs";
+import interviews from "../img/interviews";
+import quiz from "../img/quiz";
+
 function ResourceLinks() {
     return (
         <div className="resourcelinks">
             <div className="resourcelinks-hero">
               <p>What do you want to learn?</p>
               <div className="all-resoucers">
-                
+                <div>
+
+                </div>
               </div>
             </div>
         </div>
