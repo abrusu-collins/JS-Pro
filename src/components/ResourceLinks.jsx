a60fbaaefc1ac5e2d18f88236bacfd80a9cf7225
abrusu-collins <snillochemingson@gmail.com>
2022-11-06 16:19:53 +0000

    nice

--- src/components/ResourceLinks.jsx
+++ src/components/ResourceLinks.jsx
@@ -1,7 +1,12 @@
 function ResourceLinks() {
     return (
         <div className="resourcelinks">
-            
+            <div className="resourcelinks-hero">
+              <p>What do you want to learn?</p>
+              <div className="all-resoucers">
+                
+              </div>
+            </div>
         </div>
       );
 }
